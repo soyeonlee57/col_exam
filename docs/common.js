6159d65a7d0b28bb82cc8f93ac2cdfb29adba452
DDJ-Seo <dongjoo0714@gmail.com>
2021-07-16 16:10:10 +0900

    스와이퍼 슬라이드 텍스트수정

--- docs/common.js
+++ docs/common.js
@@ -1,14 +1,18 @@
 console.log("common.js 로딩")
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+function MySlider__init() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+MySlider__init();
