3476fdc5ab40a2fe70869a1ceb148aeb9169c1a5
DDJ-Seo <dongjoo0714@gmail.com>
2021-07-16 17:17:10 +0900

    페이지네이션 수정

--- docs/common.js
+++ docs/common.js
@@ -7,6 +7,35 @@ function MySlider__init() {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      renderBullet: function (index, className) {
+        let img = "";
+        
+        if ( index == 0 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=24">';
+        }
+        else if ( index == 1 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=53">';
+        }
+        else if ( index == 2 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=52">';
+        }
+        else if ( index == 3 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=21">';
+        }
+        else if ( index == 4 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=50">';
+        }
+        else if ( index == 5 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=55">';
+        }
+        else if ( index == 6 ) {
+          img = '<img src="https://source.unsplash.com/random?sig=57">';
+        }
+        
+        const bullet = '<span class="' + className + '">' + img + '</span>';
+        
+        return bullet;
+      },
     },
     navigation: {
       nextEl: ".swiper-button-next",
